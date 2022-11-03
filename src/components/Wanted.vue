1bf3a781fde01b75afd6370cef58d60a5cd97bab
croquelois <croquelois.adrien@gmail.com>
2022-11-03 23:12:57 +0100

    :wip: add frame

--- src/components/Wanted.vue
+++ src/components/Wanted.vue
@@ -2,14 +2,37 @@
 </script>
 
 <template>
-  <div class="relative w-full h-full">
-    <div class="parchment w-full h-full relative">
+  <div class="frame flex flex-col">
+    <div class="frame-border-background frame-border-x"></div>
+    <div class="flex flex-row flex-auto">
+      <div class="frame-border-background frame-border-y"></div>
+      <div class="flex-auto frame-content"></div>
+      <div class="frame-border-background frame-border-y"></div>
     </div>
-    <div class="absolute top-0 font-test">
-      <p>La capitale, il veut tuer le roi mais n’est pas la pour la couronne il veut juste assouvir sa vengeance, on peut donc le « recruter » après l’avoir battu mais un choix est alors proposé : partir maintenant avec lui conquérir la capitale ou revenir le chercher plus tard , après le combat le joueur peut avoir utiliser beaucoup de point de vie et peut vouloir rentrer se soigner et peut être améliorer son équipement, cependant le guerrier reste revendicable par l’adversaire s’il vient le défier et le vaincre.</p>
-      <p>La capitale, il veut tuer le roi mais n’est pas la pour la couronne il veut juste assouvir sa vengeance, on peut donc le « recruter » après l’avoir battu mais un choix est alors proposé : partir maintenant avec lui conquérir la capitale ou revenir le chercher plus tard , après le combat le joueur peut avoir utiliser beaucoup de point de vie et peut vouloir rentrer se soigner et peut être améliorer son équipement, cependant le guerrier reste revendicable par l’adversaire s’il vient le défier et le vaincre.</p>
-      <p>La capitale, il veut tuer le roi mais n’est pas la pour la couronne il veut juste assouvir sa vengeance, on peut donc le « recruter » après l’avoir battu mais un choix est alors proposé : partir maintenant avec lui conquérir la capitale ou revenir le chercher plus tard , après le combat le joueur peut avoir utiliser beaucoup de point de vie et peut vouloir rentrer se soigner et peut être améliorer son équipement, cependant le guerrier reste revendicable par l’adversaire s’il vient le défier et le vaincre.</p>
-      <p>La capitale, il veut tuer le roi mais n’est pas la pour la couronne il veut juste assouvir sa vengeance, on peut donc le « recruter » après l’avoir battu mais un choix est alors proposé : partir maintenant avec lui conquérir la capitale ou revenir le chercher plus tard , après le combat le joueur peut avoir utiliser beaucoup de point de vie et peut vouloir rentrer se soigner et peut être améliorer son équipement, cependant le guerrier reste revendicable par l’adversaire s’il vient le défier et le vaincre.</p>
-    </div>
+    <div class="frame-border-background frame-border-x"></div>
   </div>
 </template>
+
+<style scoped>
+.frame {
+  width: 600px;
+  height: 700px;
+  box-shadow: 8px 8px 15px rgba(0, 0, 0, 0.62);
+}
+
+.frame-content {
+  box-shadow: 0 0 50px rgba(0, 0, 0, 0.80) inset;
+}
+.frame-border-background {
+  background-color: #221812;
+}
+.frame-border-x {
+  height: 50px;
+  width: 100%;
+}
+
+.frame-border-y {
+  width: 50px;
+  height: 100%;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import GameCard from "@/components/GameCard.vue";
+import Wanted from "@/components/Wanted.vue";
 </script>
 
 <template>
   <div class="w-full flex flex-row">
-    <div class="w-1/2"></div>
+    <div class="w-1/2 p-10"><Wanted /></div>
     <div class="w-1/2 flex flex-col items-center justify-center">
       <GameCard id="game-1"/>
       <GameCard id="game-2"/>

0474d59781c59973e552864236a40ab39685867d
acroquelois <acroquelois@norauto.com>
2022-11-20 23:03:37 +0100

    :bug: syntax error

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,7 +7,7 @@ import Wanted from "@/components/Wanted.vue";
   <div class="app-container flex flex-row">
     <div class="flex-1 flex flex-col items-center justify-center"><Wanted/></div>
     <div class="flex-1 flex flex-col items-center justify-center">
-      <GameCard title="Projects" image-path="./images/idea.png"/>
+      <GameCard title="Projets" image-path="./images/idea.png"/>
       <GameCard title="Références" image-path="./images/reference.png"/>
     </div>
   </div>
